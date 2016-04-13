937183f24b34921fba5ed573b56faef1753437cd
Stephen <stephenlallum@gmail.com>
2016-04-13 10:52:29 -1000

    adds post method / incomplete

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var productsRoute = express.Router();
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
-productsRoute.get('/', function(req, res){
+productsRoute.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+productsRoute.post('/', function(req, res) {
+
+	var productData = req.body;
+	
+	fs.readFile('./db/products.js', function(err, data){
+
+		var dbData = JSON.parse(data.toString());
+		
+
+		if (err) {
+			res.send(err);
+		}
+		
+		productData.id = dbData.currentId;
+		console.log(productData);
+	});
+
+  
   res.send('ALL GOOD');
 });
+
+
 
 module.exports = productsRoute;
