62014107cfa81e2d5f866c6fc83785eb99c4b16c
Laura <lg3295@gmail.com>
2016-04-13 12:19:38 -1000

    adds post method to products/:id

--- routes/products.js
+++ routes/products.js
@@ -10,22 +10,22 @@ productsRoute.use(bodyParser.urlencoded({
 productsRoute.post('/', function(req, res) {
 
 	var productData = req.body;
-	
+
 	fs.readFile('./db/products.js', function(err, data){
 
 		var dbData = JSON.parse(data.toString());
-		
+
 		if (err) {
 			res.send(err);
 		}
 		productData.id = dbData.currentId;
-		
+
 		dbData.products.push(productData);
 		dbData.currentId++;
 		dbData = JSON.stringify(dbData);
 
 		fs.writeFile('./db/products.js', dbData, function(err){
-			
+
 			if (err) {
 				res.send(err);
 			}
@@ -34,6 +34,40 @@ productsRoute.post('/', function(req, res) {
   res.send({'success': true});
 });
 
+productsRoute.put('/:id', function(req, res){
+  var updatedData = req.body;
 
+  fs.readFile('./db/products.js', function(err, data){
+
+    var dbData = JSON.parse(data.toString());
+
+    if (err) {
+      res.send(err);
+    }
+
+    var storedObj = dbData.products[updatedData.id];
+    if (updatedData.hasOwnProperty('name')){
+      storedObj.name = updatedData.name
+    }
+    if (updatedData.hasOwnProperty('price')){
+      storedObj.price = updatedData.price
+    }
+    if (updatedData.hasOwnProperty('inventory')){
+      storedObj.inventory = updatedData.inventory
+    }
+
+    dbData = JSON.stringify(dbData);
+
+    fs.writeFile('./db/products.js', dbData, function(err){
+
+      if (err) {
+        res.send(err);
+      }
+
+    });
+
+  });
+  res.send('Thumbs up');
+});
 
 module.exports = productsRoute;
