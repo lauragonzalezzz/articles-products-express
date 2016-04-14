68dd56a007840918c3299397d2dacfa5e1f9f505
Laura <lg3295@gmail.com>
2016-04-13 20:09:26 -1000

    utilizes method override with hidden _method

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ var app = express();
 var articlesRoute = require('./routes/articles.js')
 var productsRoute = require('./routes/products.js')
 var jade = require('jade');
+var bodyParser = require('body-parser');
 var methodOverride = require('method-override')
 
 app.set('view engine', 'jade');
-app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(function(req, res, next){
+//   next();
+// });
+app.use(methodOverride(function(req, res){
+  console.log("SERVER", req.body)
+  if (req.body && typeof req.body === 'object' && '_method' in req.body){
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
 app.use('/articles', articlesRoute);
 app.use('/products', productsRoute);
 
