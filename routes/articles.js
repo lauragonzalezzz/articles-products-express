2ec26d89448417a1e13d1b0f4b420fab17022e27
Laura <lg3295@gmail.com>
2016-04-14 10:14:40 -1000

    adds post method for articles

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var articlesRoute = express.Router();
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
-articlesRoute.get('/', function(req, res){
-  res.send('ALL GOOD');
+//POST
+articlesRoute.post('/', function(req, res){
+  var article = req.body;
+  var urlTitle = encodeURIComponent(req.body.title);
+
+  fs.readFile('./db/articles.js', function(err, data){
+
+    var dbData = JSON.parse(data.toString());
+
+    if (err) {
+      res.send({'success': false});
+    }
+
+    dbData[article.title] = { 'body' : article.body, 'author' : article.author, 'urlTitle' : urlTitle }
+    dbData = JSON.stringify(dbData);
+
+    fs.writeFile('./db/articles.js', dbData, function(err){
+
+      if (err) {
+        res.send({'success': false});
+      }
+    });
+
+  });
+
+  res.send({ 'success': true });
 });
 
 module.exports = articlesRoute;
