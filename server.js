9ea130c01a0e2444b14f682a9d17094ea1228a0b
Laura <lg3295@gmail.com>
2016-04-13 16:48:34 -1000

    requires jade for templating

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var articlesRoute = require('./routes/articles.js')
 var productsRoute = require('./routes/products.js')
+var jade = require('jade');
 
+app.set('view engine', 'jade');
 app.use('/articles', articlesRoute);
 app.use('/products', productsRoute);
 
