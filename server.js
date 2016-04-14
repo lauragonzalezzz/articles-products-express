d517e12190dd7f9045527042c562203b3b7798f5
Laura <lg3295@gmail.com>
2016-04-13 19:04:42 -1000

    utilizes method override

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var app = express();
 var articlesRoute = require('./routes/articles.js')
 var productsRoute = require('./routes/products.js')
 var jade = require('jade');
+var methodOverride = require('method-override')
 
 app.set('view engine', 'jade');
+app.use(methodOverride('_method'));
 app.use('/articles', articlesRoute);
 app.use('/products', productsRoute);
 
