a54db67c2dea4915b487d5b255c5e675975edff6
Laura <lg3295@gmail.com>
2016-04-15 10:19:18 -1000

    logs all requests: date, method, url, and all headers to logs dir by date

--- middleware/logs.js
+++ middleware/logs.js
@@ -1,16 +1,29 @@
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
 function logModule(req, res, next){
   var date = new Date().toString();
-  var index = date.indexOf('2016') + 4;
-  date = date.slice(0, index);
+  var splicePoint = date.lastIndexOf(':') + 3;
+  date = date.slice(0, splicePoint);
 
   var log = {
+    "date" : date,
     "method" : req.method,
     "url" : req.url,
-    "headers" : req.headers,
-    "date" : date
+    "headers" : req.headers
   }
+  log = JSON.stringify(log, null, 2);
+  var index = date.indexOf(':');
+  var logDate = date.slice(0, index -3)
+  logDate = logDate.split(' ').join('-');
 
-  next();
+  fs.appendFile('./logs/' + logDate + '.log', log, function(err){
+    if (err){
+      return res.send({"success" : false, "error" : "Could not write to log date file" });
+    }
+    next();
+  });
 };
 
 module.exports = logModule;
