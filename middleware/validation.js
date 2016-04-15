6a6827f0d8838800fdb033e115b52d75c07c2985
Laura <lg3295@gmail.com>
2016-04-15 11:29:11 -1000

    accepts argument of necessary keys and value types, returns back to method

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,5 +1,12 @@
-function validation(req, res, next){
-
-};
+function validation(reqObj){
+  return function(req, res, next){
+    for (key in reqObj){
+      if (!req.body.hasOwnProperty(key) || typeof req.body[key] !== reqObj[key]){
+        return res.send({"success" : false, "error" : "does not have property " + key});
+      }
+    }
+    next();
+    }
+  };
 
 module.exports = validation;
