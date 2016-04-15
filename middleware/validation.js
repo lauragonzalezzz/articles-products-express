0cf8aeb127c822a7b6ec788f64910db9be6d6885
Laura <lg3295@gmail.com>
2016-04-15 13:38:03 -1000

    splits key validation and type validation

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,8 +1,13 @@
 function validation(reqObj){
   return function(req, res, next){
     for (key in reqObj){
-      if (!req.body.hasOwnProperty(key) || typeof req.body[key] !== reqObj[key]){
+      if (!req.body.hasOwnProperty(key)){
         return res.send({"success" : false, "error" : "does not have property " + key});
+      }
+      if (typeof req.body[key] !== reqObj[key]){
+        if (isNaN(req.body[key] === true)){
+          return res.send({"success" : false, "error" : key + " should be a number"})
+        }
       }
     }
     next();
