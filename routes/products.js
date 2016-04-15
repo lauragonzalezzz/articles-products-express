75dd0052c023abac47e82408db25eaf663803211
Laura <lg3295@gmail.com>
2016-04-14 14:10:24 -1000

    redirected to product.js models for requests

--- routes/products.js
+++ routes/products.js
@@ -2,99 +2,55 @@ var express = require('express');
 var productsRoute = express.Router();
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var productModule = require('../models/products.js');
 
-productsRoute.use(bodyParser.urlencoded({
-  extended: true
-}));
+productsRoute.use(bodyParser.urlencoded({extended: true}));
 
 //POST
 productsRoute.post('/', function(req, res) {
-	var productData = req.body;
-  console.log(req.body);
 
-	fs.readFile('./db/products.js', function(err, data){
+  if (!req.body.hasOwnProperty('id') ||
+    !req.body.hasOwnProperty('name') ||
+    !req.body.hasOwnProperty('price') ||
+    !req.body.hasOwnProperty('inventory')){
+    return res.send({'success': false, "Required Fields" : "Name, Id, Price, Inventory"})
+  }
+  var productData = { "id" : req.body.id, "name" : req.body.name, "price" : req.body.price, "inventory" : req.body.inventory};
 
-		var dbData = JSON.parse(data.toString());
+  productModule.add(productData, function(err){
+    if (err){
+      res.send({ "success" : false });
+    }
+  })
 
-		if (err) {
-			res.send({'success': false});
-		}
-    var id = "id" + productData.id;
-    dbData[id] = productData;
-		dbData = JSON.stringify(dbData);
-
-		fs.writeFile('./db/products.js', dbData, function(err){
-
-			if (err) {
-				res.send({'success': false});
-			}
-		});
-	});
   res.send({'success': true});
 });
 
 //PUT ID
 productsRoute.put('/:id', function(req, res){
-  var updatedData = req.body;
-  var updatedId = "id" + updatedData.id
 
-  fs.readFile('./db/products.js', function(err, data){
+  var updatedData = { "id" : req.body.id, "name" : req.body.name, "price" : req.body.price, "inventory" : req.body.inventory};
 
-    var dbData = JSON.parse(data.toString());
-
-    if (err) {
-      res.send({'success': false});
+  productModule.editByTitle(updatedData, function(err){
+    if (err){
+      return res.send({"success" : false });
     }
-
-    var storedObj = dbData[updatedId];
-    if (updatedData.hasOwnProperty('name')){
-      storedObj.name = updatedData.name
-    }
-    if (updatedData.hasOwnProperty('price')){
-      storedObj.price = updatedData.price
-    }
-    if (updatedData.hasOwnProperty('inventory')){
-      storedObj.inventory = updatedData.inventory
-    }
-
-    dbData = JSON.stringify(dbData);
-
-    fs.writeFile('./db/products.js', dbData, function(err){
-
-      if (err) {
-        res.send({'success': false});
-      }
-
-    });
-
   });
-  res.send({'success': true});
+
+  return res.send({'success': true});
 });
 
 //DELETE ID
 productsRoute.delete('/:id', function(req, res){
 
-  var productData = req.body;
+  var productId = req.params.id;
 
-  fs.readFile('./db/products.js', function(err, data){
-
-    var dbData = JSON.parse(data.toString());
-
-    if (err) {
-      res.send(err);
+  productModule.deleteById(productId, function(err){
+    if (err){
+      return res.send({"success" : false });
     }
-    var id = "id" + productData.id;
-    delete dbData[id];
-    dbData = JSON.stringify(dbData);
-
-    fs.writeFile('./db/products.js', dbData, function(err){
-
-      if (err) {
-        res.send({'success': false});
-      }
-    });
   });
-  res.send({'success': true});
+  return res.send({'success': true});
 });
 
 //GET
