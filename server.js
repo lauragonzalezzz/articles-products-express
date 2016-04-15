738d20ab24f1d50ca00b00fb77932694f1ca7889
Laura <lg3295@gmail.com>
2016-04-14 20:00:25 -1000

    uses logs middleware for logging requests

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ var articlesRoute = require('./routes/articles.js')
 var productsRoute = require('./routes/products.js')
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override')
+var logModule = require('./middleware/logs.js');
 
 app.set('view engine', 'jade');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next){
+  logModule(req, res, next);
+});
 app.use(methodOverride(function(req, res){
   if (req.body && typeof req.body === 'object' && '_method' in req.body){
     var method = req.body._method
