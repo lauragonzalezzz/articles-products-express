4ff19ebc5ac290567b5063f8ddeb74c0d4d7a95a
Laura <lg3295@gmail.com>
2016-04-15 20:28:41 -1000

    tests that a post will return a 200, incomplete

--- test/articles.spec.js
+++ test/articles.spec.js
@@ -1,21 +1,27 @@
 'use strict';
-
+const express = require('express');
 const request = require('supertest');
 const app = require('../routes/articles');
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended : true }));
 
 describe('Article Router', function(){
 
   describe('POST /', function(){
 
+    it('should', function(){
+      return true;
+    })
+
     it('should respond with a success message', function(done){
       let article = {"title" : "Laura's Awesome Article", "author" : "Laura, Duh", "body" : "Awesome Stuff"};
       request(app)
         .post('/')
-        .type('form')
+        .type('json')
+        .set('Version', "1.0")
         .send(article)
-        .expect(200, {"success" : true }, done);
+        .expect(200)
+        .end(function(err, result){
+          done();
+        });
     });
 
   }); //Ends POST
