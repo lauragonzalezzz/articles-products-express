62ab22b2be603d28a354d0e9b5000896e490fd14
Laura <lg3295@gmail.com>
2016-04-15 16:19:09 -1000

    creates public directory for client side files

---
+++ public/client.js
@@ -0,0 +1,18 @@
+var submitBtn = document.getElementById('submitBtn');
+submitBtn.addEventListener('click', function(event){
+  event.preventDefault();
+
+  var inputs = this.parentNode.querySelectorAll('input[name]');
+  var xhrUrl = document.getElementById('xhrUrl').value;
+  var payload = 'title=' + inputs[0].value + "&body=" + inputs[1].value + "&author=" + inputs[2].value + "&urlTitle=" + inputs[3].value;
+  var headerReq = new XMLHttpRequest();
+  headerReq.addEventListener('load', createRequest);
+  headerReq.open('PUT', xhrUrl);
+  headerReq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  headerReq.setRequestHeader("Version", "1.0")
+  headerReq.send(payload);
+});
+
+function createRequest(){
+  console.log(this.responseText);
+};
