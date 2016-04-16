c5823b56c77334b48d9250dc1c0adc37a1bf2652
Laura <lg3295@gmail.com>
2016-04-15 20:27:15 -1000

    checks that header has version 1.0

--- middleware/header-validation.js
+++ middleware/header-validation.js
@@ -1,11 +1,11 @@
 function headerVal(req, res, next){
   return function(req, res, next){
-    console.log('heelo');
+    console.log('req.body',req.body);
     if (req.headers.hasOwnProperty('version') && req.headers['version'] === '1.0'){
       next();
     }
     else {
-      res.send({"success" : false});
+      res.send({"success" : false, "error" : "Needs version header"});
     };
   };
 };
